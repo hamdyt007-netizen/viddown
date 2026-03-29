b2275a9d8703f613926cb60956ddd2db01244ac1
hamdyt007-netizen <hamdyt007@gmail.com>
2026-03-29 09:46:10 +0500

    Update express.js

--- express.js
+++ express.js
@@ -14,25 +14,20 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'frontend')))
 
 app.post('/download', (req, res) => {
-    const url = req.body.url
+    const { url, formatId } = req.body
+    if (!url || !formatId) {                    // ✅ add this
+        return res.status(400).json({ error: 'URL and format are required' })
+    }
 
-    if (!url) {
-        return res.status(400).json({ error: 'URL is required' })
-    }
-    try {
-        new URL(url)
-    } catch (err) {
-        return res.status(400).json({ error: 'Invalid URL. Please enter a valid YouTube link.' })
-    }
     res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"')
     res.setHeader('Content-Type', 'video/mp4')
 
-  const stream = ytDlp.execStream([
-    url,
-    '--extractor-args', 'youtube:player_client=android',
-    '--no-check-certificate',
-    '-o', '-'
-])
+    const stream = ytDlp.execStream([
+        url,
+        '--format', formatId,
+        '--no-check-certificate',
+        '-o', '-'
+    ])
 
     stream.on('error', (err) => {
         console.error('yt-dlp error:', err.message)
@@ -43,7 +38,36 @@ app.post('/download', (req, res) => {
 
     stream.pipe(res)
 })
+app.post('/formats', async (req, res) => {
+    const url = req.body.url
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000")
+
+    if (!url) {
+        return res.status(400).json({ error: 'URL is required' })
+    }
+    try {
+        new URL(url)
+    } catch (err) {
+        return res.status(400).json({ error: 'Invalid URL. Please enter a valid YouTube link.' })
+    }
+
+    try {
+        const info = await ytDlp.getVideoInfo(url)
+        const formats = info.formats
+            .filter(f => f.ext === 'mp4' && f.resolution && f.fps)  // ✅ only mp4
+            .map(f => ({
+                format_id: f.format_id,
+                ext: f.ext,
+                resolution: f.format_note,
+                fps: f.fps
+            }))
+        res.json({ formats })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: 'Failed to fetch formats' })
+    }
+})
+
+app.listen(8000, () => {
+    console.log("Server started on port 8000")
 })
